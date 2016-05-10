f478eb5ab85a4efe11493620e249b9a55bc6ae03
baldurp12 <baldurp12@ru.is>
2016-05-10 13:04:18 +0000

    parser added

--- verk5/file_parser.py
+++ verk5/file_parser.py
@@ -1,6 +1,7 @@
 import stagger
 import os
-from stagger.id3 import *  
+from stagger.id3 import *
+from pprint import pprint as pp
 
 def file_parser(file_path):
 
@@ -11,28 +12,24 @@ def file_parser(file_path):
         tag_dict['artist'] = tag.artist
         tag_dict['title'] = tag.title
         tag_dict['album'] = tag.album
-        tag_dict['track'] = tag.track
+        tag_dict['track'] = tag.track ## Skilar núll ef að ekki er til track ?
     except:
-        tag_dict = {'artist': '', 'album': '', 'title': '', 'track': ''}
+        tag_dict = {'artist': '', 'album': '', 'title': '', 'track': ''} ## If stagger can't read the ID3, we put placeholders in the values we need
 
-
-        
-##	SETJA TÓMAN STRENG EF AÐ EITTHVAÐ TAG ER EKKI TIL !
-
-
+            ## Kanski tjékka hvort að það sé hægt að finna artist i title ##
 
     if tag_dict['artist'] != '':
 ##		move_to_artist(file_path, tag_dict)
-        print(' move to artist ' + '\n' + file_path + '\n')
-        print(tag_dict)
+        print('Move to artist ' + '\n' + file_path + '\n')
+        pp(tag_dict)
     else:
-        print(' move to other ' + file_path + '\n')
-        print(tag_dict)
+        print('Move to other: ' + '\n' + file_path + '\n')
+        pp(tag_dict)
 ##		move_to_other(file_path, tag_dict)
 
 
 
 
-file_path = r"C:\Users\baldu\Dropbox\Bs.C-Hugb\4.önn\PRAL\verk4\ipod\F48\CUCH.mp3"
+file_path = r"C:\Users\baldu\Dropbox\Bs.C-Hugb\4.önn\PRAL\verk4\ipod\F29\UAVT.mp3"
 
 file_parser(file_path)
--- verk5/fix_my_ipod.py
+++ verk5/fix_my_ipod.py
@@ -2,23 +2,63 @@ import os
 from os import path as pa
 from os.path import join as jo
 from stagger import *
+from pprint import pprint as pp
+
 def list_dir_test():
+    multi_pass_check = []
+    
     a = [x[0] for x in os.walk(os.getcwd())]
     test_list = []
     for i in a[1:]:
         for j in os.listdir(i):
             curr_path = pa.join(i,j)
             if pa.isfile(curr_path) and j.endswith('.mp3'):
-                try:
-                    tag = stagger.read_tag(curr_path)
-                    test_list.append(tag.artist)
-                except:
-                    print(curr_path)
+##                if j in multi_pass_check:
+##                    print('\n' + j + ' is already parsed' + '\n and is in: ' + curr_path)
+##                else:
+##                    multi_pass_check.append(j)
+                file_parser(curr_path)
 
-    print(set(test_list))
-    
+
+
+##                try:
+##                    tag = stagger.read_tag(curr_path)
+##                    test_list.append(tag.artist)
+##                except:
+##                    print(curr_path)
+##
+##    print(set(test_list))
+##    
         
         
+
+
+def file_parser(file_path):
+
+##	smíða dictionary með stagger:
+    tag_dict = {}
+    try:
+        tag = stagger.read_tag(file_path)
+        tag_dict['artist'] = tag.artist
+        tag_dict['title'] = tag.title
+        tag_dict['album'] = tag.album
+        tag_dict['track'] = tag.track ## Skilar núll ef að ekki er til track ?
+    except:
+        tag_dict = {'artist': '', 'album': '', 'title': '', 'track': ''} ## If stagger can't read the ID3, we put placeholders in the values we need
+
+            ## Kanski tjékka hvort að það sé hægt að finna artist i title ##
+
+    if tag_dict['artist'] != '':
+##		move_to_artist(file_path, tag_dict)
+        print('Move to artist ' + '\n' + file_path + '\n')
+        pp(tag_dict)
+    else:
+        print('Move to other: ' + '\n' + file_path + '\n')
+        pp(tag_dict)
+##		move_to_other(file_path, tag_dict)
+
+
 
 
 list_dir_test()
+
