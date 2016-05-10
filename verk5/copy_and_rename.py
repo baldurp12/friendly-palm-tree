e5fe930e91403433137d35f1e79dd44113126676
baldurp12 <baldurp12@ru.is>
2016-05-10 17:09:48 +0000

    a

--- verk5/copy_and_rename.py
+++ verk5/copy_and_rename.py
@@ -1,10 +1,19 @@
+import os
+
 def copy_and_rename(final_path, file_path, file_dict):
 
-	if title exists:
-		rename 'Track number' ' - ' + 'Title' + '.mp3'
 
-	else:
-		þarf ekkert að gera
+        print(os.path.split("path_to_file")[0])
 
-	
-	copy(file_path, final_path)
+
+
+
+##
+##	if title exists:
+##		rename 'Track number' ' - ' + 'Title' + '.mp3'
+##
+##	else:
+##		þarf ekkert að gera
+##
+##	
+##	copy(file_path, final_path)
