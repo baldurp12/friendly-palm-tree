8e740fadc1eeadc0570ebfecca0b2191acd68e2c
baldurp12 <baldurp12@ru.is>
2016-05-10 17:56:54 +0000

    If and try

--- verk5/fix_my_ipod.py
+++ verk5/fix_my_ipod.py
@@ -4,8 +4,27 @@ from os.path import join as jo
 from stagger import *
 from pprint import pprint as pp
 
+copy_folder = r'00_copy_"#$%%(folder'
+other_folder = '00_Other'
+
 def list_dir_test():
-    multi_pass_check = []
+    multi_pass_check = [] ## Hluti af testi, ma henda i tiltekt
+    
+    ##----MAKE COPY FOLDER AND OTHER FOLDER----##
+
+    try:
+        os.mkdir(os.path.join(os.getcdw(), copy_folder))
+    except:
+        print('Could not create the folder: ' + copy_folder)
+        print('enter " to continue ? (y / yes)')
+        user_choice = input()
+        if user_choice == ('y'):
+            pass
+        else:
+            return 'User aborted'
+    
+    print('ooooops')
+    return 7000
     
     a = [x[0] for x in os.walk(os.getcwd())]
     test_list = []
@@ -13,11 +32,12 @@ def list_dir_test():
         for j in os.listdir(i):
             curr_path = pa.join(i,j)
             if pa.isfile(curr_path) and j.endswith('.mp3'):
+                pass
 ##                if j in multi_pass_check:
 ##                    print('\n' + j + ' is already parsed' + '\n and is in: ' + curr_path)
 ##                else:
 ##                    multi_pass_check.append(j)
-                file_parser(curr_path)
+####                file_parser(curr_path)
 
 
 
@@ -36,6 +56,7 @@ def list_dir_test():
 def file_parser(file_path):
 
 ##	smíða dictionary með stagger:
+    
     tag_dict = {}
     try:
         tag = stagger.read_tag(file_path)
@@ -57,6 +78,37 @@ def file_parser(file_path):
         pp(tag_dict)
 ##		move_to_other(file_path, tag_dict)
 
+
+
+def move_to_artist(file_path, tag_dict):
+
+        ##----if we have multiple artists----##   
+    
+    if type(tag_dict['artist']) == list:
+        just_album_path = os.path.join( 'asdf', tag_dict['album'])
+        #print(just_album_path)
+        if os.path.isdir(just_album_path):
+            pass
+        else:
+            os.mkdir(just_album_path)
+        ## copy_and_rename(just_album_path, file_path, dict)
+        ##----if we have one artist----##
+    else:
+        artist_path = os.path.join( 'asdf' , tag_dict['artist'])
+        os.mkdir(artist_path)
+        if tag_dict['album'] != "":
+                ##---- if we have an aritst and an album----##
+            
+            ##print(album_path)
+            album_path = os.path.join(new_path, os.path.join(tag_dict['artist'], tag_dict['album']))
+            if os.path.isdir(album_path):
+                pass
+            else:
+                os.mkdir(album_path)
+                    ## copy_and_rename(album_path, file_path, dict)
+                ## -----if we just have an artist but no album-----##
+        ##else:
+                ## copy_and_rename(artist_path, file_path, dict) 
 
 
 
