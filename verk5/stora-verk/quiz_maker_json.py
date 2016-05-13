411efd25f81c6499a40848eb6c4581498db95eea
baldurp12 <baldurp12@ru.is>
2016-05-13 14:33:04 +0000

    Tiltekt

--- verk5/stora-verk/quiz_maker_json.py
+++ verk5/stora-verk/quiz_maker_json.py
@@ -3,42 +3,50 @@ import os
 from os import path
 from pprint import pprint as pp
 
-spurningar = r"C:\Users\baldur\Dropbox\Bs.C-Hugb\4.önn\PRAL\verk4\friendly-palm-tree\verk5\stora-verk\animal-spurningar.txt"
-svor = r"C:\Users\baldur\Dropbox\Bs.C-Hugb\4.önn\PRAL\verk4\friendly-palm-tree\verk5\stora-verk\animal-svör.txt"
-
-
 def quiz_maker_2000():
-    a = open(spurningar, 'r', encoding='utf-8')
-    b = open(svor, 'r', encoding='utf-8')
 
 
-    topic = "animals"
+    q_fold = sorted(os.listdir(os.path.join(os.getcwd(), 'questions')))
+    a_fold = sorted(os.listdir(os.path.join(os.getcwd(), 'answers')))
+
+    print(q_fold)
+
+    topics = []
+    for i in q_fold:
+        topics.append(i.split(sep='-'))
+        
+    print(topics)
     
-    x = a.read().splitlines()
-    y = b.read().splitlines()
+##    a = open(spurningar, 'r', encoding='utf-8')
+##    b = open(svor, 'r', encoding='utf-8')
+##
+##
+##    json_writer = open('tester.json', 'a', encoding='utf-8')
+##
+##    
+##
+##    
+##    x = a.read().splitlines()
+##    y = b.read().splitlines()
+##
+##    
+##    test_dict = {topic: []}
+##
+##    
+##    for i in range( len(x) ):
+##        test_dict[topic].append({ str( "q" + str(i+1) + "-question" ): str(x[i]) })
+##        test_dict[topic].append({ str( "q" + str(i+1) + "-answer" ): str(y[i]) })
+##
+##    abc = json.dumps(test_dict)
+##    
+##
+##
+##    
+##
+##    c.write(abc)
+##    print(json.loads(abc))
 
-    test_dict = {topic: []}
 
-    
-    for i in range( len(x) ):
-        test_dict[topic].append({ str( "q" + str(i+1) + "-question" ): str(x[i]) })
-        test_dict[topic].append({ str( "q" + str(i+1) + "-answer" ): str(y[i]) })
-
-    abc = json.dumps(test_dict)
-    
-
-
-    c = open('tester.json', 'w', encoding='utf-8')
-
-    c.write(abc)
-    print(json.loads(abc))
-
-
-
-    
-
-    
-    
 
         
 quiz_maker_2000()
---
+++ verk5/stora-verk/testing_field.py
@@ -0,0 +1,7 @@
+def test_et():
+    
+    a= "animal-svör.txt".split(sep='-')
+    print(a[0])
+
+
+test_et()
