1dc3ec37372261e6c2bd5adfbb5c914d5e9a7c1f
baldurp12 <baldurp12@ru.is>
2016-05-13 16:20:18 +0000

    JSON GUÐ!

--- Verkefni5-seinni/BaldurFikt/quiz_maker_json.py
+++ Verkefni5-seinni/BaldurFikt/quiz_maker_json.py
@@ -6,16 +6,50 @@ from pprint import pprint as pp
 def quiz_maker_2000():
 
 
-    q_fold = sorted(os.listdir(os.path.join(os.getcwd(), 'questions')))
-    a_fold = sorted(os.listdir(os.path.join(os.getcwd(), 'answers')))
 
-    print(q_fold)
+    
+    question_path = os.path.join(os.getcwd(), 'questions')
+    answers_path = os.path.join(os.getcwd(), 'answers')
+    
+    q_files = sorted(os.listdir(question_path))
+    a_files = sorted(os.listdir(answers_path))
 
+
+    print(q_files)
     topics = []
-    for i in q_fold:
-        topics.append(i.split(sep='-'))
+    
+    for i in q_files:
+        topics.append(i.split(sep='-')[0])
+
+    test_dict = {}
+    
+    for ind, top in enumerate(topics):
+        a = open(os.path.join(question_path, q_files[ind]), 'r', encoding='utf-8')
+        b = open(os.path.join(answers_path, a_files[ind]), 'r', encoding='utf-8')
+        x = a.read().splitlines()
+        y = b.read().splitlines()
+
+        a.close()
+        b.close()
         
-    print(topics)
+        test_dict[top] = []
+        for i in range( len(x) ):
+            test_dict[top].append({ str( "q" + str(i+1) + "-question" ): str(x[i]) })
+            test_dict[top].append({ str( "q" + str(i+1) + "-answer" ): str(y[i]) })
+
+    abc = json.dumps(test_dict)
+
+    json_writer = open('tester.json', 'w', encoding='utf-8')
+
+    json_writer.write(abc)
+    
+
+
+                    
+                
+        
+        
+
     
 ##    a = open(spurningar, 'r', encoding='utf-8')
 ##    b = open(svor, 'r', encoding='utf-8')
