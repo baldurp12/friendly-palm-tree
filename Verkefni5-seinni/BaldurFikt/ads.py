ca29b933bd3b60d548f7c1983c77fe6a9328cd8b
BaldurM <baldurp12@ru.is>
2016-05-15 17:04:44 +0000

    Aðalvirkni komin

--- Verkefni5-seinni/BaldurFikt/ads.py
+++ Verkefni5-seinni/BaldurFikt/ads.py
@@ -1,5 +1,4 @@
 import tkinter
-<<<<<<< HEAD
 import string
 import json
 
@@ -7,11 +6,7 @@ import json
 
 counter = -1
 list_test=[x for x in (string.ascii_letters + string.digits) ]
-=======
 
-counter = 0
-list_test=['a','b','c','d','e','f','g','h','i','j']
->>>>>>> 2e8043371dfb8d1e0978807a346d91bdd6f761ed
 
 window = tkinter.Tk()
 window.title("Baldur did it")
@@ -19,13 +14,8 @@ window.geometry("300x300")
 def callback():
     global counter
     counter += 1
-<<<<<<< HEAD
     if counter >= len(list_test):
         counter = 0
-=======
-    if counter >= 10:
-        newwind()
->>>>>>> 2e8043371dfb8d1e0978807a346d91bdd6f761ed
     lbl.configure(text=list_test[counter])
 
 ##for i in range(10):
@@ -34,7 +24,7 @@ def callback():
 ##    lbl.pack(side=tkinter.TOP)
 ##    btn.pack(side=tkinter.TOP)
 
-<<<<<<< HEAD
+
 ##
 ##def newwind():
 ##    popwindow = tkinter.Tk()
@@ -44,17 +34,6 @@ def callback():
 ##    butt.pack()
 ##    window.mainloop()
 
-def var_states():
-   print("male: %d,\nfemale: %d" % (var1.get(), var2.get()))
-
-Label(window, text="Your sex:").grid(row=0, sticky=W)
-var1 = IntVar()
-Checkbutton(window, text="male", variable=var1).grid(row=1, sticky=W)
-var2 = IntVar()
-Checkbutton(window, text="female", variable=var2).grid(row=2, sticky=W)
-Button(window, text='Quit', command=master.quit).grid(row=3, sticky=W, pady=4)
-Button(window, text='Show', command=var_states).grid(row=4, sticky=W, pady=4)
-=======
 
 def newwind():
     popwindow = tkinter.Tk()
@@ -64,7 +43,6 @@ def newwind():
     butt.pack()
     window.mainloop()
 
->>>>>>> 2e8043371dfb8d1e0978807a346d91bdd6f761ed
 
 window.wm_iconbitmap('favicon.ico')
 
@@ -79,12 +57,6 @@ ent.pack()
 btn.pack()
 
 
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 2e8043371dfb8d1e0978807a346d91bdd6f761ed
 window.mainloop()
 
 
--- Verkefni5-seinni/GamanSaman/tkinter.py
+++ Verkefni5-seinni/GamanSaman/tkinter.py
@@ -7,23 +7,14 @@ from os import path
 
 counter = 0
 s = ""
-quiz_dict = {}
+
+
 class Window(Frame):
     def __init__(self, master = None):
         Frame.__init__(self, master)
         self.master = master
         self.init_window()
-        if os.path.isfile('QandA.json'):
-            global quiz_dict
-            with open("QandA.json", 'r', encoding='utf-8') as f:
-                quiz_dict = json.load(f)
-        else:
-            quiz_maker_2000()
-            global quiz_dict
-            with open("QandA.json", 'r', encoding='utf-8') as f:
-                quiz_dict = json.load(f)
-
-
+        
 ##    def showImg(self):
 ##        load = Image.open('my_smiley.jpg')
 ##        render = ImageTk.PhotoImage(load)
@@ -38,7 +29,7 @@ class Window(Frame):
     def client_exit(self):
         exit()
     def init_window(self):
-        self.master.title("Quiz for kids")
+        self.master.title("Quiz for kids and Hjalti")
         
         self.pack(fill = BOTH, expand=1)
         
@@ -54,10 +45,19 @@ class Window(Frame):
         ##-------add image-----------
         load = Image.open('hjalti.jpg')
         render = ImageTk.PhotoImage(load)
-
+        
         img = Label(self, image = render)
         img.image = render
         img.place(x=330, y=5)
+        ##------build quiz dictionary
+        self.quiz_dict = {}
+        if os.path.isfile('QandA.json'):
+            pass
+        else:
+            self.quiz_maker_2000()
+        with open("QandA.json", 'r', encoding='utf-8') as f:
+            self.quiz_dict = json.load(f)
+        self.topic_list = sorted(list(self.quiz_dict))        
         ##----text---------
         text = Label(self, text = "Welcome to our educating Quiz for kids!!!")
         text.pack()
@@ -68,38 +68,74 @@ class Window(Frame):
         text.place(x=50, y=30)
 
         ##-------checkbutton-------
+        self.buttonvalue = StringVar()
 
-        self.var = IntVar()
-        a = Checkbutton(self, text="Animals",
-            variable=self.var,
-            command=self.animal)
-        a.pack()
-        a.place(x=50, y=60)
+        for i in range(len(self.topic_list)):
+            R = Radiobutton(self, text=self.topic_list[i].title(), variable=self.buttonvalue,
+                            command=self.buttonvalue.set(self),
+                            value=self.topic_list[i])
+            R.pack()
+            R.place(x=50, y = 60 + i*35)
+            
 
-        self.var = IntVar()
-        b = Checkbutton(self, text="Human",
-            variable=self.var,
-            command=self.human)
-        b.pack()
-        b.place(x=50, y=90)
 
-        self.var = IntVar()
-        c = Checkbutton(self, text="Planet",
-            variable=self.var,
-            command=self.planet)
-        c.pack()
-        c.place(x=50, y=120)
 
-        self.var = IntVar()
-        d = Checkbutton(self, text="Space",
-            variable=self.var,
-            command=self.space)
-        d.pack()
-        d.place(x=50, y=150)
+##        R1 = Radiobutton(self, text="Animal", variable=self.var2, value='animal',
+##                          command=self.var2.set(self))
+##        R1.pack()
+##        R1.place(x=50, y=60)
+##
+##        R2 = Radiobutton(self, text="Human", variable=self.var2, value='human',
+##                          command=self.var2.set(self))
+##        R2.pack()
+##        R2.place(x=50, y=90)
+##
+##        R3 = Radiobutton(self, text="Space", variable=self.var2, value='space',
+##                          command=self.var2.set(self))
+##        R3.pack()
+##        R3.place(x=50, y=120)
+##
+##        R4 = Radiobutton(self, text="Planets", variable=self.var2, value='planet',
+##                          command=self.var2.set(self))
+##        R4.pack()
+##        R4.place(x=50, y=150)
+
+
+##
+##
+##
+##        self.var = IntVar()
+##        a = Checkbutton(self, text="Animals",
+##            variable=self.var,
+##            command=self.animal)
+##        a.pack()
+##        a.place(x=50, y=60)
+##
+##        self.var = IntVar()
+##        b = Checkbutton(self, text="Human",
+##            variable=self.var,
+##            command=self.human)
+##        b.pack()
+##        b.place(x=50, y=90)
+##
+##        self.var = IntVar()
+##        c = Checkbutton(self, text="Planet",
+##            variable=self.var,
+##            command=self.planet)
+##        c.pack()
+##        c.place(x=50, y=120)
+##
+##        self.var = IntVar()
+##        d = Checkbutton(self, text="Space",
+##            variable=self.var,
+##            command=self.space)
+##        d.pack()
+##        d.place(x=50, y=150)
 
 
         ##-----EnterButton----
-        enterButton = Button(self, text = "Make Quiz", command=self.questions)
+
+        enterButton = Button(self, text = "Make Quiz", command = self.questions)
         
         enterButton.pack()
         enterButton.place(x=50, y=200)
@@ -109,8 +145,15 @@ class Window(Frame):
     def questions(self):
         global text
         global s
+        global counter
+        counter = 0
+        self.current_topic = self.buttonvalue.get()
+        
         ##-----question-----
-        text = Label(self, text = animal["question01"])
+        try:
+            text = Label(self, text = self.quiz_dict[self.current_topic]['question01'])
+        except:
+            text = Label(self, text = "No topic selected")
         text.place(x=50, y=230)
   
         
@@ -129,8 +172,8 @@ class Window(Frame):
     def callback(self):
         global counter
         ##global s
-        counter += 1
-        if self.usertext.get() == animal["answer" + str(counter).zfill(2)]:
+  
+        if self.usertext.get() == self.quiz_dict[self.current_topic]["answer" + str(counter + 1).zfill(2)]:
             correct_text = Label(self, text = "You are corrent!, good job!")
             correct_text.place(x=50, y=320)
             
@@ -140,55 +183,97 @@ class Window(Frame):
             img.image = render
             img.place(x=300, y=350)
         else:
-            wrong_text = Label(self, text = "You are incorrent, sorry")
+            wrong_text = Label(self, text = "You are an idiot, not sorry")
             wrong_text.place(x=50, y=320)
+            wrong_text = Label(self, text = 'Correct answer is: \n' + self.quiz_dict[self.current_topic]["answer" + str(counter+1).zfill(2)])
+            wrong_text.place(x=50, y=420)
             
             load = Image.open('rangtsvar.jpg')
             render = ImageTk.PhotoImage(load)
             img = Label(self, image = render)
             img.image = render
             img.place(x=300, y=350)
-        text.configure(text=animal["question" + str(counter).zfill(2)])
+        counter += 1
+        text.configure(text=self.quiz_dict[self.current_topic]["question" + str(counter+1).zfill(2)])  
+
+
+        
+
 
 ##    def compare_ans(self):
 ##        print(v.get())
-        
-    def animal(self):
-            print("Þu ert flottur!")
-            
-    def human(self):
-            print("OH BABY!")
-    def planet(self):
-            print("OOOOOJEEEEE!")
-    def space(self):
-            print("BaldurSkvaldurKaldurFlaldur!")
 
-animal= {
-    "answer01": "Bamboo",
-    "answer02": "False",
-    "answer03": "Arachnophobia",
-    "answer04": "The tiger",
-    "answer05": "True",
-    "answer06": "Yes",
-    "answer07": "Cows",
-    "answer08": "True",
-    "answer09": "Antarctica",
-    "answer10": "True",
-    "question01": "What is the closest planet to the Sun?",
-    "question02": "What is the name of the 2nd biggest planet in our solar system?",
-    "question03": "What is the hottest planet in our solar system?",
-    "question04": "What planet is famous for its big red spot on it?",
-    "question05": "What planet is famous for the beautiful rings that surround it?",
-    "question06": "Can humans breathe normally in space as they can on Earth?",
-    "question07": "Is the sun a star or a planet?",
-    "question08": "Who was the first person to walk on the moon?",
-    "question09": "What planet is known as the red planet?",
-    "question10": "What is the name of the force holding us to the Earth?"}
+##    def radio_update():
+##        pass
+##        
+##    def animal(self):
+##            print("Þu ert flottur!")
+##    def human(self):
+##            print("OH BABY!")
+##    def planet(self):
+##            print("OOOOOJEEEEE!")
+##    def space(self):
+##            print("BaldurSkvaldurKaldurFlaldur!")
 
+
+
+
+    def quiz_maker_2000(self):
+        question_path = os.path.join(os.getcwd(), 'questions')
+        answers_path = os.path.join(os.getcwd(), 'answers')
         
+        q_files = sorted(os.listdir(question_path))
+        a_files = sorted(os.listdir(answers_path))
+
+        topics = []
        
+        for i in q_files:
+            topics.append(i.split(sep='-')[0])
+
+        q_a_dict = {}
         
-    
+        for index, top in enumerate(topics):
+            a = open(os.path.join(question_path, q_files[index]), 'r', encoding='utf-8')
+            b = open(os.path.join(answers_path, a_files[index]), 'r', encoding='utf-8')
+            x = a.read().splitlines()
+            y = b.read().splitlines()
+
+            a.close()
+            b.close()
+            
+            temp_dict = {}
+
+            for i in range( len(x) ): ## len(x) ætti að vera == len(y)
+                
+                temp_dict[str('question' + str(i+1).zfill(2))] = str(x[i])
+                temp_dict[str('answer' + str(i+1).zfill(2))] = str(y[i])
+
+            q_a_dict[top] = temp_dict
+
+        with open("QandA.json", "w", encoding='utf_8') as js:
+            json.dump(q_a_dict,js, indent=4, sort_keys=True)
+##
+##animal= {
+##    "answer01": "Bamboo",
+##    "answer02": "False",
+##    "answer03": "Arachnophobia",
+##    "answer04": "The tiger",
+##    "answer05": "True",
+##    "answer06": "Yes",
+##    "answer07": "Cows",
+##    "answer08": "True",
+##    "answer09": "Antarctica",
+##    "answer10": "True",
+##    "question01": "What is the closest planet to the Sun?",
+##    "question02": "What is the name of the 2nd biggest planet in our solar system?",
+##    "question03": "What is the hottest planet in our solar system?",
+##    "question04": "What planet is famous for its big red spot on it?",
+##    "question05": "What planet is famous for the beautiful rings that surround it?",
+##    "question06": "Can humans breathe normally in space as they can on Earth?",
+##    "question07": "Is the sun a star or a planet?",
+##    "question08": "Who was the first person to walk on the moon?",
+##    "question09": "What planet is known as the red planet?",
+##    "question10": "What is the name of the force holding us to the Earth?"}
 
 root = Tk()
 root.resizable(width=FALSE, height=TRUE)
@@ -196,39 +281,4 @@ root.geometry("500x600")
 root.wm_iconbitmap('turtles.ico')
 app = Window(root)
 root.mainloop()
-
-def quiz_maker_2000():
-    question_path = os.path.join(os.getcwd(), 'questions')
-    answers_path = os.path.join(os.getcwd(), 'answers')
-    
-    q_files = sorted(os.listdir(question_path))
-    a_files = sorted(os.listdir(answers_path))
-
-    topics = []
-   
-    for i in q_files:
-        topics.append(i.split(sep='-')[0])
-
-    q_a_dict = {}
-    
-    for index, top in enumerate(topics):
-        a = open(os.path.join(question_path, q_files[index]), 'r', encoding='utf-8')
-        b = open(os.path.join(answers_path, a_files[index]), 'r', encoding='utf-8')
-        x = a.read().splitlines()
-        y = b.read().splitlines()
-
-        a.close()
-        b.close()
-        
-        temp_dict = {}
-
-        for i in range( len(x) ): ## len(x) ætti að vera == len(y)
-            
-            temp_dict[str('question' + str(i+1).zfill(2))] = str(x[i])
-            temp_dict[str('answer' + str(i+1).zfill(2))] = str(y[i])
-
-        q_a_dict[top] = temp_dict
-
-    with open("QandA.json", "w", encoding='utf_8') as js:
-        json.dump(q_a_dict,js, indent=4, sort_keys=True)
 
