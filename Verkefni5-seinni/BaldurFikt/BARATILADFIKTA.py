07d84d957c510a686bd180630329042981337515
BaldurM <baldurp12@ru.is>
2016-05-15 18:58:24 +0000

    ads

--- Verkefni5-seinni/BaldurFikt/BARATILADFIKTA.py
+++ Verkefni5-seinni/BaldurFikt/BARATILADFIKTA.py
@@ -13,25 +13,25 @@ var = StringVar()
 
 R1 = Radiobutton(root, text="Animal", variable=var, value='Animal',
                   command=sel)
-R1.grid(row=0, sticky=W)
+R1.grid(row=0, column=0)
 
 R2 = Radiobutton(root, text="Human", variable=var, value='Human',
                   command=sel)
-R2.grid(row=1, sticky=W)
+R2.grid(row=0, column=1)
 
 R3 = Radiobutton(root, text="Space", variable=var, value='Space',
                   command=sel)
-R3.grid(row=2, sticky=W)
+R3.grid(row=1, column=0)
 
 R4 = Radiobutton(root, text="Planets", variable=var, value='Planets',
                   command=sel)
-R4.grid(row=3, sticky=W)
+R4.grid(row=1, column=1)
 
 
 
 label = Label(root, text='Select an option')
 
-label.grid(sticky=W)
+label.grid(row=2, column=2)
 
 
 
